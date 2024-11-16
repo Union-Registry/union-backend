9e276f54cd474a1fd1fb376b16053fd07af1ebcc
mhmdksh <eddsnx3@gmail.com>
2024-11-16 12:20:34 +0700

    Initialized Union Registry Subgraph

---
+++ src/civil-registry.ts
@@ -0,0 +1,83 @@
+import {
+  Initialized as InitializedEvent,
+  OwnershipTransferred as OwnershipTransferredEvent,
+  UnionAccepted as UnionAcceptedEvent,
+  UnionProposed as UnionProposedEvent,
+  UnionRevoked as UnionRevokedEvent
+} from "../generated/CivilRegistry/CivilRegistry"
+import {
+  Initialized,
+  OwnershipTransferred,
+  UnionAccepted,
+  UnionProposed,
+  UnionRevoked
+} from "../generated/schema"
+
+export function handleInitialized(event: InitializedEvent): void {
+  let entity = new Initialized(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.version = event.params.version
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleOwnershipTransferred(
+  event: OwnershipTransferredEvent
+): void {
+  let entity = new OwnershipTransferred(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.previousOwner = event.params.previousOwner
+  entity.newOwner = event.params.newOwner
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleUnionAccepted(event: UnionAcceptedEvent): void {
+  let entity = new UnionAccepted(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.unionId = event.params.unionId
+  entity.attestationUid = event.params.attestationUid
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleUnionProposed(event: UnionProposedEvent): void {
+  let entity = new UnionProposed(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.unionId = event.params.unionId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
+
+export function handleUnionRevoked(event: UnionRevokedEvent): void {
+  let entity = new UnionRevoked(
+    event.transaction.hash.concatI32(event.logIndex.toI32())
+  )
+  entity.unionId = event.params.unionId
+
+  entity.blockNumber = event.block.number
+  entity.blockTimestamp = event.block.timestamp
+  entity.transactionHash = event.transaction.hash
+
+  entity.save()
+}
---
+++ tests/civil-registry-utils.ts
@@ -0,0 +1,101 @@
+import { newMockEvent } from "matchstick-as"
+import { ethereum, BigInt, Address } from "@graphprotocol/graph-ts"
+import {
+  Initialized,
+  OwnershipTransferred,
+  UnionAccepted,
+  UnionProposed,
+  UnionRevoked
+} from "../generated/CivilRegistry/CivilRegistry"
+
+export function createInitializedEvent(version: BigInt): Initialized {
+  let initializedEvent = changetype<Initialized>(newMockEvent())
+
+  initializedEvent.parameters = new Array()
+
+  initializedEvent.parameters.push(
+    new ethereum.EventParam(
+      "version",
+      ethereum.Value.fromUnsignedBigInt(version)
+    )
+  )
+
+  return initializedEvent
+}
+
+export function createOwnershipTransferredEvent(
+  previousOwner: Address,
+  newOwner: Address
+): OwnershipTransferred {
+  let ownershipTransferredEvent = changetype<OwnershipTransferred>(
+    newMockEvent()
+  )
+
+  ownershipTransferredEvent.parameters = new Array()
+
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam(
+      "previousOwner",
+      ethereum.Value.fromAddress(previousOwner)
+    )
+  )
+  ownershipTransferredEvent.parameters.push(
+    new ethereum.EventParam("newOwner", ethereum.Value.fromAddress(newOwner))
+  )
+
+  return ownershipTransferredEvent
+}
+
+export function createUnionAcceptedEvent(
+  unionId: BigInt,
+  attestationUid: BigInt
+): UnionAccepted {
+  let unionAcceptedEvent = changetype<UnionAccepted>(newMockEvent())
+
+  unionAcceptedEvent.parameters = new Array()
+
+  unionAcceptedEvent.parameters.push(
+    new ethereum.EventParam(
+      "unionId",
+      ethereum.Value.fromUnsignedBigInt(unionId)
+    )
+  )
+  unionAcceptedEvent.parameters.push(
+    new ethereum.EventParam(
+      "attestationUid",
+      ethereum.Value.fromUnsignedBigInt(attestationUid)
+    )
+  )
+
+  return unionAcceptedEvent
+}
+
+export function createUnionProposedEvent(unionId: BigInt): UnionProposed {
+  let unionProposedEvent = changetype<UnionProposed>(newMockEvent())
+
+  unionProposedEvent.parameters = new Array()
+
+  unionProposedEvent.parameters.push(
+    new ethereum.EventParam(
+      "unionId",
+      ethereum.Value.fromUnsignedBigInt(unionId)
+    )
+  )
+
+  return unionProposedEvent
+}
+
+export function createUnionRevokedEvent(unionId: BigInt): UnionRevoked {
+  let unionRevokedEvent = changetype<UnionRevoked>(newMockEvent())
+
+  unionRevokedEvent.parameters = new Array()
+
+  unionRevokedEvent.parameters.push(
+    new ethereum.EventParam(
+      "unionId",
+      ethereum.Value.fromUnsignedBigInt(unionId)
+    )
+  )
+
+  return unionRevokedEvent
+}
